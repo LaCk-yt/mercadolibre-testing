56cd323dd61f2f8b62b0f7874492bb6e8e49bb36
LaCk-yt <62211842+LaCk-yt@users.noreply.github.com>
2023-11-27 21:08:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios").default;
 
-const appId = "6153761841854455"
-const clientSecret = "dPe8pEV1dPYcGyqOrtBqgF7Ewe33oq2I"
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true })); // Add urlencoded parser
